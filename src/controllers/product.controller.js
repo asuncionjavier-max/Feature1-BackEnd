445721ef63e0a855657b48652b4fc4a38a416522
Javi <asuncionjavier96@gmail.com>
2026-05-16 22:34:10 +0200

    Añadimos el resto de controllores continuamos con routes

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -20,3 +20,46 @@ export const getAllProductsById = (req,res) =>{
     data:product
 })
 }
+export const createProduct = (req, res) =>{
+    const {name,price} = req.body
+    if(!name || price === undefined || price > 0) return  res.status(404).json({
+        ok: false,
+        error: { message: "Error al crear el producto" }})
+
+    const newProduct = productService.createProduct({name,price})
+    return status(201).json({
+        ok:true,
+        data: newProduct,
+    })
+
+}
+
+export const updateProduct = (req,res) =>{
+    const {id} = req.params
+    const {data} = req.body
+    const update = productService.updateProduct(id,data)
+
+    if(!update) return res.status(404).json({
+        ok: false,
+        error: { message: "Producto no encontrado" }})
+
+    return res.status(200).json({
+    ok: true,
+    data:product
+    })    
+}
+
+export const deleteProduct = (req,res) =>{
+    const {id} = req.params;
+
+    const deleted = productService.deleteProduct(id);
+
+    if(!deleted) return res.status(404).json({
+        ok: false,
+        error: { message: "Producto no encontrado" }})
+
+    return res.status(200).json({
+    ok: true,
+    data: deleted
+ })
+}
